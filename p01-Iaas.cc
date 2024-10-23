ef50afbcfde82b0acff4f2abf76c81c133915548
Alejandro <alu0101718767@ull.edu.es>
2024-10-23 09:39:51 +0000

    commit inicial

---
+++ p01-Iaas.cc
@@ -0,0 +1,21 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Alejandro Padilla
+ * @date 26 Jun 2026
+ * @brief The classical 'Hello World' first program in any language
+ *
+ * @see http://www.cplusplus.com/doc/tutorial/program_structure/
+ * @see http://cpp.sh/2dd
+ */
+
+#include <iostream>
+
+int main() {
+  // Is this your first C++ program ?
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
